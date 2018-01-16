134655e3b1f90375ce9320d57c6b895ebcd53ee7
xingkong906 <xingkong906@gmail.com>
2018-01-16 18:06:46 +0800

    修正关键字错误

--- test/re_test.py
+++ test/re_test.py
@@ -1,5 +1,5 @@
 import re
 
 pattern = re.compile(r'>\$(.*?)<')
-str = '<h4 class="set-width">$6,000<br/><small>goal</small></h4>'
-print(pattern.search(str).group(1))
+txt = '<h4 class="set-width">$6,000<br/><small>goal</small></h4>'
+print(pattern.search(txt).group(1))
