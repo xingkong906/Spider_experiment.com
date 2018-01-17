915359311ec87bae565f543cf5992c26053a609b
xingkong906 <xingkong906@gmail.com>
2018-01-17 21:42:36 +0800

    默认的

--- dao/Projects.py
+++ dao/Projects.py
@@ -20,25 +20,27 @@ def get(text):
     soup = BeautifulSoup(text, 'lxml')
     projects = soup.find_all('div', attrs={'class': 'project-card'})
     for project in projects:
-        try
-        rs['id'] = to_int(project['id'])
-        rs['href'] = project.a['href']
-        rs['title'] = clean(str(project.find('h3', attrs={'class': 'project-title'}).a.string))
-        footer = project.find('div', attrs={'class': 'project-card-footer'})
-        rs['researcher'] = footer.div.a.string
-        rs['institution'] = footer.find('div', attrs={'class', 'institution'}).a.string
-        rs['url_researcher'] = footer.div.a['href']
-        researcher['name'] = rs['researcher']
-        researcher['url'] = rs['url_researcher']
-        status = footer.find('div', attrs={'class', 'row stats-row'})
-        rs['funding_percent'] = to_int(h4_founding(status.select("#funding-percent")[0].h4)) / 100
-        rs['funding_goal'] = to_int(h4_goal(status.select("#funding-goal")[0].h4))
-        rs['count_lab_notes'] = to_int(h4_founding(status.select(".time-remaining")[0].h4))
-        # 写入数据库
-        dao_project.item['data'] = rs
-        dao_researchers.item['data'] = researcher
-        dao_project.insert()
-        dao_researchers.insert()
+        try:
+            rs['id'] = to_int(project['id'])
+            rs['href'] = project.a['href']
+            rs['title'] = clean(str(project.find('h3', attrs={'class': 'project-title'}).a.string))
+            footer = project.find('div', attrs={'class': 'project-card-footer'})
+            rs['researcher'] = footer.div.a.string
+            rs['institution'] = footer.find('div', attrs={'class', 'institution'}).a.string
+            rs['url_researcher'] = footer.div.a['href']
+            researcher['name'] = rs['researcher']
+            researcher['url'] = rs['url_researcher']
+            status = footer.find('div', attrs={'class', 'row stats-row'})
+            rs['funding_percent'] = to_int(h4_founding(status.select("#funding-percent")[0].h4)) / 100
+            rs['funding_goal'] = to_int(h4_goal(status.select("#funding-goal")[0].h4))
+            rs['count_lab_notes'] = to_int(h4_founding(status.select(".time-remaining")[0].h4))
+            # 写入数据库
+            dao_project.item['data'] = rs
+            dao_researchers.item['data'] = researcher
+            dao_project.insert()
+            dao_researchers.insert()
+        except Exception as e:
+            print(e)
         # 清空
         rs.clear()
         researcher.clear()
