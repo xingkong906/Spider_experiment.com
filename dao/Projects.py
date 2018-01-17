a459d2cf3006a8b4f339abf3f7f2e0790670de1b
xingkong906 <xingkong906@gmail.com>
2018-01-17 21:39:09 +0800

    默认的

--- dao/Projects.py
+++ dao/Projects.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from util.stringUtil import *
 from dao.Dao import Dao
+import time
 
 home_page = r"https://experiment.com"
 url = r'https://experiment.com/discover/more'
@@ -12,11 +13,14 @@ url = r'https://experiment.com/discover/more'
 # cursor = con.cursor()
 # cursor.execute("created")
 def get(text):
+    dao_project = Dao(table='project')
+    dao_researchers = Dao(table='researchers')
     rs = {}
     researcher = {}
     soup = BeautifulSoup(text, 'lxml')
     projects = soup.find_all('div', attrs={'class': 'project-card'})
     for project in projects:
+        try
         rs['id'] = to_int(project['id'])
         rs['href'] = project.a['href']
         rs['title'] = clean(str(project.find('h3', attrs={'class': 'project-title'}).a.string))
@@ -30,7 +34,14 @@ def get(text):
         rs['funding_percent'] = to_int(h4_founding(status.select("#funding-percent")[0].h4)) / 100
         rs['funding_goal'] = to_int(h4_goal(status.select("#funding-goal")[0].h4))
         rs['count_lab_notes'] = to_int(h4_founding(status.select(".time-remaining")[0].h4))
-    return [rs, researcher]
+        # 写入数据库
+        dao_project.item['data'] = rs
+        dao_researchers.item['data'] = researcher
+        dao_project.insert()
+        dao_researchers.insert()
+        # 清空
+        rs.clear()
+        researcher.clear()
 
 
 def h4_founding(text):
@@ -46,20 +57,21 @@ def h4_goal(text):
     return str(match.group(1))
 
 
-def do():
-    dao_project = Dao('project')
-    dao_researchers = Dao('researchers')
-    for i in range(1, 128):
-        req = requests.get(url, params={'offset': '12', 'order': 'founded'})
+def do(start, end):
+    times = time.time()
+    for i in range(start, end + 1):
+        req = requests.get(url, params={'offset': i, 'order': 'founded'})
         html = json.loads(req.text)['cards']
-        project, researcher = get(html)
-        dao_project.se
+        print(html)
+        get(html)
+    print("完成%d" % times.time() - start)
 
 
 if __name__ == '__main__':
     # html = open('../data/project.html', 'r').read()
-    url = r'https://experiment.com/discover/more'
-    req = requests.get(url, params={'offset': '762', 'order': 'founded'})
-    html = json.loads(req.text)['cards']
-    print(html)
-    get(html)
+    # url = r'https://experiment.com/discover/more'
+    # req = requests.get(url, params={'offset': '762', 'order': 'founded'})
+    # html = json.loads(req.text)['cards']
+    # print(html)
+    # get(html)
+    do(1, 128)
