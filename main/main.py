e6f4a9a518b59d8de3daa47829340d7230e9dc84
starrysky108 <xingkong906@gmail.com>
2018-01-30 19:32:37 +0800

    增加多线程支持

--- main/main.py
+++ main/main.py
@@ -1,8 +1,11 @@
 from dao.backer import Backer
 from dao.project_page import ProjectPage
 from dao.Dao import Dao
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
 
 index = 'https://experiment.com'
+root = ' '
 
 
 def select_project():
@@ -12,19 +15,44 @@ def select_project():
     print(len(rs))
     for cell in rs:
         id, url = cell
+        print(id)
         page = ProjectPage(id=id, url=index + url)
         page.run()
 
 
 def select_backer():
-    pass
+    dao = Dao()
+    sql = 'SELECT id,username FROM backers WHERE "process_status"="false"'
+    rs = dao.cursor.execute(sql).fetchall()
+    print(len(rs))
+    return rs
+    # for cell in rs:
+    #     id, username = cell
+    #     print(id)
+    #     backer = Backer(id=id, url=root + username)
+    #     backer.run()
 
 
-def run():
-    pass
+def main(task):
+    ids, username = task
+    print(ids)
+    backer = Backer(id=ids, url=root + username)
+    backer.run()
+    print(id)
+
+
+def run(tasks):
+    # select_project()
+    # select_backer()
+    with ThreadPoolExecutor(max_workers=10) as execctor:
+        execctor.map(main, tasks, timeout=100)
+    print("处理完成")
 
 
 if __name__ == '__main__':
     # 项目主程序
-    run()
-    select_project()
+    # run()
+    # select_project()
+    tasks = select_backer()
+    run(tasks)
+    print("已经完成")
