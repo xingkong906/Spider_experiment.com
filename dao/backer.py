4683e424de825a2fddfa87b233480cf03e565be6
starrysky108 <xingkong906@gmail.com>
2018-01-30 19:34:19 +0800

    修正部分错误

--- dao/backer.py
+++ dao/backer.py
@@ -20,35 +20,58 @@ class Backer(object):
         self.selector = self.etree.HTML(self.html)
         self.dao = Dao()
 
+    @staticmethod
+    def func(selector, xpath, order=-1):
+        rs = None
+        try:
+            rs = selector.xpath(xpath)
+        except IndexError as e:
+            logger.error("可能不存在，请检查\t" + e)
+            rs = ""
+        except Exception as e:
+            logger.error(e)
+            rs = ""
+        finally:
+            if rs and order >= 0:
+                return rs[order]
+            else:
+                if not rs:
+                    return ''
+                return rs
+
     def run(self):
-        self.backer['lab_notes_count'] = self.selector.xpath('//*[@id="updates-nav"]/a/h3/span/text()')[0]
-        self.backer['comment_count'] = self.selector.xpath('//*[@id="comments-nav"]/a/h3/span/text()')[0]
-        self.backer['started_count'] = self.selector.xpath('//*[@id="created_projects-nav"]/a/h3/span/text()')[0]
-        self.backer['backed_count'] = self.selector.xpath('//*[@id="backed_projects-nav"]/a/h3/span/text()')[0]
+        self.backer['lab_notes_count'] = self.func(self.selector, xpath='//*[@id="updates-nav"]/a/h3/span/text()',
+                                                   order=0)
+        self.backer['comment_count'] = self.func(self.selector, xpath='//*[@id="comments-nav"]/a/h3/span/text()',
+                                                 order=0)
+        self.backer['started_count'] = self.func(self.selector,
+                                                 xpath='//*[@id="created_projects-nav"]/a/h3/span/text()',
+                                                 order=0)
+        self.backer['backed_count'] = self.func(self.selector, xpath='//*[@id="backed_projects-nav"]/a/h3/span/text()',
+                                                order=0)
         self.get_projects_backed()
         self.get_projects_started()
-        self.dao.uodate(table='backers', data=self.backer)
-        self.dao.close()
+        self.dao.update(key='id', value=self.id, table='backers', data=self.backer)
+        self.dao.update(key="id", value=self.id, table='backers', data={'process_status': "true"})
 
     def get_projects_started(self):
-        selector = self.selector.xpath('.//*[@id="created_projects"]')[0]
-        self.backer['started'] = Backer.get_projects_card(selector)
+        selector = self.func(self.selector, xpath='.//*[@id="created_projects"]', order=0)
+        self.backer['started'] = self.get_projects_card(selector)
 
     def get_projects_backed(self):
-        selector = self.selector.xpath('.//*[@id="backed_projects"]')[0]
-        self.backer['backed'] = Backer.get_projects_card(selector)
+        selector = self.func(self.selector, xpath='.//*[@id="backed_projects"]', order=0)
+        self.backer['backed'] = self.get_projects_card(selector)
 
-    @staticmethod
-    def get_projects_card(node):
+    def get_projects_card(self, node):
         # 解析card
         rs = {}
         temp = []
         try:
             for cell in node.xpath('.//*[@class="project-card"]'):
                 rs.clear()
-                rs['id'] = to_int(cell.xpath('./@id')[0])
-                rs['url'] = cell.xpath('./div[2]/h3/a/@href')[0]
-                rs['title'] = cell.xpath('./div[2]/h3/a/text()')[0]
+                rs['id'] = to_int(self.func(cell, xpath='./@id', order=0))
+                rs['url'] = self.func(cell, xpath='./div[2]/h3/a/@href', order=0)
+                rs['title'] = self.func(cell, xpath='./div[2]/h3/a/text()', order=0)
                 temp.append(str(rs['id']) + '\t' + rs['title'] + '\t' + rs['url'])
             return '\n'.join(temp)
         except Exception:
